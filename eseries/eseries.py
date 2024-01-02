cf32e241bc252875a7be4267b3d8a1789ce1964b
Günther Jena <guenther.jena@laseratwork.com>
2024-01-02 09:03:56 +0100

    cleanuo

--- eseries/eseries.py
+++ eseries/eseries.py
@@ -3,46 +3,62 @@ from typing import Tuple, Generator
 from math import log10, floor
 
 
-_BASE_LOG = 2
-
-
 class ESeries:
     _instances = []
 
-    def __init__(self, name: str, tolerance: float, base_values: Tuple[int, ...]):
+    def __init__(self, name: str, tolerance: float, base_exponent: int, base_values: Tuple[int, ...]):
         self.name = name
         self.tolerance = tolerance
+        self.base_exponent = base_exponent
         self.base_values = base_values
         ESeries._instances.append(self)
     
     @classmethod
     def get_instances(cls) -> Tuple['ESeries']:
+        """ Get a tuple with all the ESeries instances.
+
+        >>> ESeries.get_instances()
+        (E3, E6, E12, E24, E48, E96, E192)
+        """
         return tuple(cls._instances)
     
-    def __str__(self) -> str:
+    def __repr__(self) -> str:
         return self.name
-
-    def _iterate_from(self, value: float, upwards: bool = True) -> Generator[float, None, None]:
-        exponent_offset = floor(log10(value) - _BASE_LOG)
-        mantissa = value / (10 ** exponent_offset)
-        
-        if upwards:
-            index = bisect_left(self.base_values, mantissa)
-        else:
-            index = bisect_right(self.base_values, mantissa) - 1
+    
+    def iterate_up(self, value: float) -> Generator[float, None, None]:
+        exponent_offset = floor(log10(value) - self.base_exponent)
+        index = bisect_left(self.base_values, value / (10 ** exponent_offset))
             
         while True:
             if index >= len(self.base_values):
                 index = 0
-                exponent_offset += 1 if upwards else -1
+                exponent_offset += 1
 
             if exponent_offset < 0:
                 # improving floating point result by doing division or multiplication depending on exponent_offset
                 yield self.base_values[index] / (10 ** -exponent_offset)
             else:    
                 yield self.base_values[index] * (10 ** exponent_offset)
-            index += 1 if upwards else -1
 
+            index += 1
+    
+    def iterate_down(self, value: float) -> Generator[float, None, None]:
+        exponent_offset = floor(log10(value) - self.base_exponent)
+        index = bisect_right(self.base_values, value / (10 ** exponent_offset)) - 1
+            
+        while True:
+            if index < 0:
+                index = len(self.base_values) - 1
+                exponent_offset -= 1
+
+            if exponent_offset < 0:
+                # improving floating point result by doing division or multiplication depending on exponent_offset
+                yield self.base_values[index] / (10 ** -exponent_offset)
+            else:    
+                yield self.base_values[index] * (10 ** exponent_offset)
+
+            index -= 1
+        
     def find_greater_than_or_equal(self, value: float) -> float:
         """Find the smallest value greater-than or equal-to the given value.
 
@@ -52,7 +68,7 @@ class ESeries:
             The smallest value from the specified series which is greater-than
             or equal-to the query value.
         """
-        return next(self._iterate_from(value))
+        return next(self.iterate_up(value))
 
     def find_greater_than(self, value: float) -> float:
         """Find the smallest value greater-than or equal-to the given value.
@@ -63,7 +79,7 @@ class ESeries:
             The smallest value from the specified series which is greater-than
             the query value.
         """
-        return next(v for v in self._iterate_from(value) if v != value)
+        return next(v for v in self.iterate_up(value) if v != value)
 
     def find_less_than_or_equal(self, value: float) -> float:
         """Find the largest value less-than or equal-to the given value.
@@ -74,7 +90,7 @@ class ESeries:
             The largest value from the specified series which is less-than
             or equal-to the query value.
         """
-        return next(self._iterate_from(value, upwards=False))
+        return next(self.iterate_down(value))
 
     def find_less_than(self, value):
         """Find the largest value less-than or equal-to the given value.
@@ -85,7 +101,7 @@ class ESeries:
             The largest value from the specified series which is less-than
             the query value.
         """
-        return next(v for v in self._iterate_from(value, upwards=False) if v != value)
+        return next(v for v in self.iterate_down(value) if v != value)
 
     def __lt__(self, other: float) -> float:
         return self.find_less_than(other)
@@ -114,21 +130,19 @@ class ESeries:
     def __eq__(self, value: float) -> float:
         return self.find_nearest(value)
         
-    def range(self, start: float, stop: float, open: bool = True):
+    def range(self, start: float, stop: float, include_stop: bool = False) -> Generator[float, None, None]:
         """Generate E values in a range inclusive of the start and stop values.
 
         :param start: The beginning of the range. The yielded values may include this value.
         :param stop: The end of the range. The yielded values may include this value.
+        :param include_stop: If True, the yielded values may include the stop value.
 
         :yields:
             Values from the specified range which lie between the start and stop
-            values inclusively, and in order from lowest to highest.
+            values, and in order from lowest to highest.
         """
-        g = self._iterate_from(start)
-        
-        while True:
-            value = next(g)
-            if (open and value >= stop) or (not open and value > stop):
+        for value in self.iterate_up(start):
+            if (include_stop and value > stop) or (not include_stop and value >= stop):
                 return
             yield value
     
@@ -170,32 +184,32 @@ class ESeries:
         return (value - value * self.tolerance, value + value * self.tolerance)
 
 
-E3 = ESeries( 'E3', 0.4, (100, 220, 470) )
-E6 = ESeries( 'E6', 0.2, (100, 150, 220, 330, 470, 680) )
-E12 = ESeries( 'E12', 0.1, (100, 120, 150, 180, 220, 270, 330, 390, 470, 560, 680, 820) )
-E24 = ESeries( 'E24', 0.05, (100, 110, 120, 130, 150, 160, 180, 200, 220, 240, 270, 300, 330, 360, 390, 430, 470, 510,
-                             560, 620, 680, 750, 820, 910) )
-E48 = ESeries( 'E48', 0.02, (100, 105, 110, 115, 121, 127, 133, 140, 147, 154, 162, 169, 178, 187, 196, 205, 215, 226,
-                             237, 249, 261, 274, 287, 301, 316, 332, 348, 365, 383, 402, 422, 442, 464, 487, 511, 536,
-                             562, 590, 619, 649, 681, 715, 750, 787, 825, 866, 909, 953) )
-E96 = ESeries( 'E96', 0.01, (100, 102, 105, 107, 110, 113, 115, 118, 121, 124, 127, 130, 133, 137, 140, 143, 147, 150,
-                             154, 158, 162, 165, 169, 174, 178, 182, 187, 191, 196, 200, 205, 210, 215, 221, 226, 232,
-                             237, 243, 249, 255, 261, 267, 274, 280, 287, 294, 301, 309, 316, 324, 332, 340, 348, 357,
-                             365, 374, 383, 392, 402, 412, 422, 432, 442, 453, 464, 475, 487, 499, 511, 523, 536, 549,
-                             562, 576, 590, 604, 619, 634, 649, 665, 681, 698, 715, 732, 750, 768, 787, 806, 825, 845,
-                             866, 887, 909, 931, 953, 976) )
-E192 = ESeries( 'E192', 0.005, (100, 101, 102, 104, 105, 106, 107, 109, 110, 111, 113, 114, 115, 117, 118, 120, 121,
-                                123, 124, 126, 127, 129, 130, 132, 133, 135, 137, 138, 140, 142, 143, 145, 147, 149,
-                                150, 152, 154, 156, 158, 160, 162, 164, 165, 167, 169, 172, 174, 176, 178, 180, 182,
-                                184, 187, 189, 191, 193, 196, 198, 200, 203, 205, 208, 210, 213, 215, 218, 221, 223,
-                                226, 229, 232, 234, 237, 240, 243, 246, 249, 252, 255, 258, 261, 264, 267, 271, 274,
-                                277, 280, 284, 287, 291, 294, 298, 301, 305, 309, 312, 316, 320, 324, 328, 332, 336,
-                                340, 344, 348, 352, 357, 361, 365, 370, 374, 379, 383, 388, 392, 397, 402, 407, 412,
-                                417, 422, 427, 432, 437, 442, 448, 453, 459, 464, 470, 475, 481, 487, 493, 499, 505,
-                                511, 517, 523, 530, 536, 542, 549, 556, 562, 569, 576, 583, 590, 597, 604, 612, 619,
-                                626, 634, 642, 649, 657, 665, 673, 681, 690, 698, 706, 715, 723, 732, 741, 750, 759,
-                                768, 777, 787, 796, 806, 816, 825, 835, 845, 856, 866, 876, 887, 898, 909, 920, 931,
-                                942, 953, 965, 976, 988) )
+E3 = ESeries('E3', 0.4, 2, (10, 22, 47) )
+E6 = ESeries('E6', 0.2, 2, (10, 15, 22, 33, 47, 68) )
+E12 = ESeries('E12', 0.1, 2, (10, 12, 15, 18, 22, 27, 33, 39, 47, 56, 68, 82) )
+E24 = ESeries('E24', 0.05, 2, (10, 11, 12, 13, 15, 16, 18, 20, 22, 24, 27, 30, 33, 36, 39, 43, 47, 51, 56, 62, 68, 75,
+                               82, 91) )
+E48 = ESeries('E48', 0.02, 3, (100, 105, 110, 115, 121, 127, 133, 140, 147, 154, 162, 169, 178, 187, 196, 205, 215,
+                               226, 237, 249, 261, 274, 287, 301, 316, 332, 348, 365, 383, 402, 422, 442, 464, 487,
+                               511, 536, 562, 590, 619, 649, 681, 715, 750, 787, 825, 866, 909, 953) )
+E96 = ESeries('E96', 0.01, 3, (100, 102, 105, 107, 110, 113, 115, 118, 121, 124, 127, 130, 133, 137, 140, 143, 147,
+                               150, 154, 158, 162, 165, 169, 174, 178, 182, 187, 191, 196, 200, 205, 210, 215, 221,
+                               226, 232, 237, 243, 249, 255, 261, 267, 274, 280, 287, 294, 301, 309, 316, 324, 332,
+                               340, 348, 357, 365, 374, 383, 392, 402, 412, 422, 432, 442, 453, 464, 475, 487, 499,
+                               511, 523, 536, 549, 562, 576, 590, 604, 619, 634, 649, 665, 681, 698, 715, 732, 750,
+                               768, 787, 806, 825, 845, 866, 887, 909, 931, 953, 976) )
+E192 = ESeries('E192', 0.005, 3, (100, 101, 102, 104, 105, 106, 107, 109, 110, 111, 113, 114, 115, 117, 118, 120, 121,
+                                  123, 124, 126, 127, 129, 130, 132, 133, 135, 137, 138, 140, 142, 143, 145, 147, 149,
+                                  150, 152, 154, 156, 158, 160, 162, 164, 165, 167, 169, 172, 174, 176, 178, 180, 182,
+                                  184, 187, 189, 191, 193, 196, 198, 200, 203, 205, 208, 210, 213, 215, 218, 221, 223,
+                                  226, 229, 232, 234, 237, 240, 243, 246, 249, 252, 255, 258, 261, 264, 267, 271, 274,
+                                  277, 280, 284, 287, 291, 294, 298, 301, 305, 309, 312, 316, 320, 324, 328, 332, 336,
+                                  340, 344, 348, 352, 357, 361, 365, 370, 374, 379, 383, 388, 392, 397, 402, 407, 412,
+                                  417, 422, 427, 432, 437, 442, 448, 453, 459, 464, 470, 475, 481, 487, 493, 499, 505,
+                                  511, 517, 523, 530, 536, 542, 549, 556, 562, 569, 576, 583, 590, 597, 604, 612, 619,
+                                  626, 634, 642, 649, 657, 665, 673, 681, 690, 698, 706, 715, 723, 732, 741, 750, 759,
+                                  768, 777, 787, 796, 806, 816, 825, 835, 845, 856, 866, 876, 887, 898, 909, 920, 931,
+                                  942, 953, 965, 976, 988) )
 
 
 def series_from_name(name) -> ESeries:
